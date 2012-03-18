f9ae19fc1fe685706446890acb557baa043e4d1b
psupreme <clinton.bale@gmail.com>
2012-03-18 16:34:38 -0400

    Default project

---
+++ src/jac444b.a2/src/jac444b/a2/Program.java
@@ -0,0 +1,15 @@
+package jac444b.a2;
+
+/**
+ *
+ * @author psupreme
+ */
+public class Program {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
